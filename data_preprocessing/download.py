abbf74c2691f25b5085bf82c7eb6d5340cd1891c
Jonas Welt <jonas.welt@outlook.com>
2026-06-09 23:33:55 +0200

    add sources

--- data_preprocessing/download.py
+++ data_preprocessing/download.py
@@ -3,8 +3,16 @@ import requests
 import zipfile
 from pathlib import Path
 
-ZIP_URL = "https://zenodo.org/records/18772136/files/dataset.zip"
 DATA_PATH = Path(__file__).parent / ".." / "datasets" / "raw"
+
+ALFS_URL = "https://zenodo.org/records/18772136/files/dataset.zip"
+
+RGB_URL = "https://zenodo.org/records/19034999/files/images_rgb.zip"
+LABEL_RGB_URL = "https://zenodo.org/records/19034999/files/labels_rgb.zip"
+
+THERMAL_URL = "https://zenodo.org/records/19034999/files/images_thermal.zip"
+LABELS_THERMAL_URL = "https://zenodo.org/records/19034999/files/labels_thermal.zip"
+
 
 def download_zip(zip_url: str, save_path: Path, skip_if_exists: bool = True):
     if save_path.exists() and skip_if_exists:
@@ -29,14 +37,26 @@ def extract_zip(zip_file_path: Path, extract_to: Path, skip_if_exists: bool = Tr
         zip_ref.extractall(extract_to)
     print(f"Extracted to: {extract_to}")
 
-def download_and_extract_zip(data_dir: Path, zip_url: str, skip_if_exists: bool = True):
-    zip_file_path = data_dir / "downloaded_file.zip"
-    extracted_dir_path = data_dir / "extracted_files"
+def download_and_extract_zip(data_dir: Path, subdir: str, image_url: str, label_url: str, skip_if_exists: bool = True):
+    file_name = "images" if label_url is None else "data"
     
-    os.makedirs(data_dir, exist_ok=True)
-    download_zip(zip_url, zip_file_path, skip_if_exists)
+    zip_file_path = data_dir / subdir / f"{file_name}_file.zip"
+    extracted_dir_path = data_dir / subdir / f"{file_name}_files"
+
+    os.makedirs(data_dir / subdir, exist_ok=True)
+    download_zip(image_url, zip_file_path, skip_if_exists)
 
     os.makedirs(extracted_dir_path, exist_ok=True)
     extract_zip(zip_file_path, extracted_dir_path, skip_if_exists)
+    
+    if label_url:
+        label_zip_file_path = data_dir / subdir / f"labels_file.zip"
+        label_extracted_dir_path = data_dir / subdir / f"labels_files"
 
-download_and_extract_zip(DATA_PATH, ZIP_URL)
+        download_zip(label_url, label_zip_file_path, skip_if_exists)
+        os.makedirs(label_extracted_dir_path, exist_ok=True)
+        extract_zip(label_zip_file_path, label_extracted_dir_path, skip_if_exists)
+
+download_and_extract_zip(DATA_PATH, "alfs_data", ALFS_URL, None)
+download_and_extract_zip(DATA_PATH, "rgb_data", RGB_URL, LABEL_RGB_URL)
+download_and_extract_zip(DATA_PATH, "thermal_data", THERMAL_URL, LABELS_THERMAL_URL)
